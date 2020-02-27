e61d9e4bd62fb3d9bcbd47920083b3aa3f5620a3
slim <smedini@gmail.com>
2020-02-27 13:01:49 +0100

    search properties

--- routes/properties.js
+++ routes/properties.js
@@ -5,10 +5,19 @@ var properties = col(process.env.PROPERTIES_COLLECTION_NAME)
 var debug = require('debug')('api:properties')
 
 router.get('/', (req, res) => {
+    const pageSize = req.query.ps ? Number(req.query.ps) : globalPageSize
+    const pageNumber = req.query.pn ? Number(req.query.pn) : 0
     const filter = {}
+
+    if (req.query.n) {
+      const text = req.query.n
+      filter.$text = { $search: text }
+    }
 
     properties
       .find(filter)
+      .skip(pageNumber * pageSize)
+      .limit(pageSize)
       .toArray()
       .then((properties) => res.send(properties))
   },
